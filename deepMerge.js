e2471578b67c8aa896c42ac21b65f70c6d60a8f1
aappleton8 <aappleton8@users.noreply.github.com>
2020-10-14 21:59:49 +0100

    Start the deep merge function.
    Signed-off-by: aappleton8 <aappleton8@users.noreply.github.com>

--- deepMerge.js
+++ deepMerge.js
@@ -1,1 +1,13 @@
-// Add deep merge code here 
+module.exports = function deepMergeIf(destination, source) {
+	if (destination === source) return destination 
+  for (let i in source) {
+    let sourceValue = source[i] 
+    let destinationValue = destination[i]
+    if ((Object(sourceValue) === sourceValue) && (Object(destinationValue) === destinationValue)) {
+      destination[i] = deepAssign(destinationValue, sourceValue)
+      continue
+    }
+    destination[i] = source[i]
+  }
+  return destination
+}
