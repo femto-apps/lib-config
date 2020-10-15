8b40613ecb3b597bcd77e34f389e91aa0d952cdc
aappleton8 <aappleton8@users.noreply.github.com>
2020-10-15 21:54:27 +0100

    Finish the deep merge script.
    Signed-off-by: aappleton8 <aappleton8@users.noreply.github.com>

--- deepMerge.js
+++ deepMerge.js
@@ -1,13 +1,19 @@
-module.exports = function deepMergeIf(destination, source) {
+_ = require('lodash')
+module.exports = function deepMergeIf(destination, source, options) {
 	if (destination === source) return destination 
   for (let i in source) {
     let sourceValue = source[i] 
     let destinationValue = destination[i]
     if ((Object(sourceValue) === sourceValue) && (Object(destinationValue) === destinationValue)) {
-      destination[i] = deepAssign(destinationValue, sourceValue)
+      destination[i] = deepMergeIf(destinationValue, sourceValue, options)
       continue
     }
-    destination[i] = source[i]
+    console.log(Array.isArray(destination), i, options.addToDestinationArray)
+    if (((_.has(destination, i)) || (options.addToDestinationArray && (Array.isArray(destination))) || (options.addToDestination && !(Array.isArray(destination)))) 
+    	&& (options.overwriteWithUndefined || source[i] !== undefined || (Array.isArray(destination))) 
+    	&& (options.overwriteWithNull || source[i] !== null || (Array.isArray(destination)))) {
+    	console.log(i)
+    	_.set(destination, i, source[i]) }
   }
   return destination
 }
--- test/index.js
+++ test/index.js
@@ -1,5 +1,43 @@
 const config = require('../')
+const deepMerge = require('../deepMerge')
 
+let d = Date.now()
+console.log(d)
+let a = {
+  "a": "b",
+  "c": 34,
+  "d": [4, 5, 6],
+  "e": {
+    "f": "g",
+    "h": [7, 8]
+  },
+  "i": "j",
+  "m": "n", 
+  "o": "p", 
+  "r": "s",
+  "q": d
+}
+let b = {
+  "a": "b2",
+  "c": 35, 
+  "d": [4, 9, 6, 7, null, undefined, 10],
+  "e": {
+    "f": "g2"
+  }, 
+  "k": "ll",
+  "m": null,
+  "n": undefined,
+  "o": undefined,
+  "r": {
+    "t": "u", 
+    "v": "w"
+  },
+  "q": Date.now()
+}
+
+console.log(deepMerge(a, b, {addToDestination: false, addToDestinationArray: true, overwriteWithNull: false, overwriteWithUndefined: true}))
+
+/*
 console.log(config.get('hello'))
 console.log(config.get('bye'))
 
@@ -19,5 +57,6 @@ console.log(config.get('this.path.does.not.exist'))
 
 console.log(config.values())
 
-config.setOutDir('./')
-config.save('config3')
+//config.setOutDir('./')
+//config.save('config3')
+*/
